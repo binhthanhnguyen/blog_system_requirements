a187b8ce8c176283260b0297c2cbcaaf68925f9e
Nguyen <nguyen.thanh.binh@framgia.com>
2015-06-10 17:02:41 +0700

    Finish user edit, update, index actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -18,15 +23,33 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-
+  def edit
+    @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
+  
 
   private
     def user_params
       params.require(:user).permit :name, :email, :password, :password_confirmation
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
