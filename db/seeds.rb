769f875ee876244ee41a8b81cbf62203ebcf17c8
Nguyen <nguyen.thanh.binh@framgia.com>
2015-06-11 22:57:06 +0700

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,10 @@ users = User.order(:created_at).take(6)
   body = Faker::Lorem.sentence(5)
   users.each { |user| user.entries.create!(body: body, title: title) }
 end
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
